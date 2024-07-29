043b5759238c331a8b025210ec72cb76dbdfca69
Taynam Benichio <taynambenichio@gmail.com>
2024-07-29 15:33:32 +0100

    ✨ Changed marker icon

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import { BoltMarketMap } from "./map/map";
 
 export default function App() {
   return (
-    <h1 className="w-full h-screen flex items-center justify-center">
+    <h1 className="flex flex-col items-center justify-center w-full h-screen">
       <BoltMarketMap />
+      <h1 className='text-xl font-bold'>Tips:</h1>
+      <p><strong className='text-green-400'>Green</strong>: 2km radius</p>
+      <p><strong className='text-amber-500'>Orange</strong>: 5km radius</p>
+      <p><strong className='text-red-600'>Red</strong>: 10km radius</p>
     </h1>
   )
 }
--- src/map/map.tsx
+++ src/map/map.tsx
@@ -1,10 +1,46 @@
 'use client'
+import L, { LatLngExpression } from 'leaflet';
 import "leaflet/dist/leaflet.css";
 import { useState } from 'react';
 import { Circle, LayerGroup, MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import store from '../assets/store.svg';
+
+interface LocationMarketInterface {
+  pos: LatLngExpression
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onMove: any
+}
+
+function LocationMarker({ pos, onMove }: LocationMarketInterface) {
+  const StoreIcon = new L.Icon({
+    iconUrl: store,
+    iconRetinaUrl: store,
+    popupAnchor: [-0, -0],
+    iconSize: [20, 20],
+
+  });
+
+  return (
+    <Marker
+      position={pos}
+      draggable
+      autoPan
+      icon={StoreIcon}
+      eventHandlers={{
+        moveend: (event) => {
+          onMove([event.target.getLatLng().lat, event.target.getLatLng().lng]);
+        }
+      }}
+    >
+      <Popup>
+        <strong>Location of Store</strong>
+      </Popup>
+    </Marker>
+  );
+}
 
 export function BoltMarketMap() {
-  const [mapCenter] = useState({ lat: 38.75977, lng: -9.26002 })
+  const [mapCenter, setMapCenter] = useState({ lat: 38.75977, lng: -9.26002 })
 
   return (
     <MapContainer
@@ -16,17 +52,25 @@ export function BoltMarketMap() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={mapCenter}>
-        <Popup>
-          Location of Store
-        </Popup>
-      </Marker>
+
+      {/* <div onClick={toggleDraggable}>
+        <Marker
+          draggable={draggable}
+          eventHandlers={eventHandlers}
+          position={position}
+          ref={markerRef}
+          icon={StoreIcon}>
+          <Popup>
+            <strong>Location of Store</strong>
+          </Popup>
+        </Marker>
+      </div> */}
+      <LocationMarker onMove={setMapCenter} pos={mapCenter} />
       <LayerGroup>
         <Circle
           center={mapCenter}
-          pathOptions={{ color: 'green', fillColor: 'green' }}
-          radius={2000}
-        />
+          pathOptions={{ color: '#47BE85', fillColor: '#47BE85' }}
+          radius={2000} />
         <Circle
           center={mapCenter}
           pathOptions={{ color: 'orange', fillColor: 'orange' }}
